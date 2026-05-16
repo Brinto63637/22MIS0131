4f78353a5cfa44744f6254747684d58df2a94c8c
Brinto63637 <brintojenith@gmail.com>
2026-05-16 18:04:50 +0530

    Stage 7 completed frontend and backend implementation

--- notification_app_be/priority_notifications.js
+++ notification_app_be/priority_notifications.js
@@ -6,7 +6,7 @@ async function getNotifications() {
       "http://4.224.186.213/evaluation-service/notifications",
       {
         headers: {
-          Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJicmludG9qZW5pdGhAZ21haWwuY29tIiwiZXhwIjoxNzc4OTMzMzQ1LCJpYXQiOjE3Nzg5MzI0NDUsImlzcyI6IkFmZm9yZCBNZWRpY2FsIFRlY2hub2xvZ2llcyBQcml2YXRlIExpbWl0ZWQiLCJqdGkiOiI4YzJlNGM5NC1jM2IxLTQ3MmQtOTQ2NS1mOGNkNDEwMTcxZjAiLCJsb2NhbGUiOiJlbi1JTiIsIm5hbWUiOiJicmludG8gamVuaXRoIGIiLCJzdWIiOiI4YzMwNjRjYi04MWQyLTQwNGYtOGY3MC04ZDE3ZDdkZmE3MWIifSwiZW1haWwiOiJicmludG9qZW5pdGhAZ21haWwuY29tIiwibmFtZSI6ImJyaW50byBqZW5pdGggYiIsInJvbGxObyI6IjIybWlzMDEzMSIsImFjY2Vzc0NvZGUiOiJTZkZ1V2ciLCJjbGllbnRJRCI6IjhjMzA2NGNiLTgxZDItNDA0Zi04ZjcwLThkMTdkN2RmYTcxYiIsImNsaWVudFNlY3JldCI6IlVDWVFjRHpWQkNTS2RTSmUifQ.Fn6VOaVu4wqwY24Tem8GEPeovxHXL62_-u6BrdV4zZI"
+            Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
         }
       }
     );
